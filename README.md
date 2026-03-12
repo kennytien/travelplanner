eb351734835830877720280ff450e9c4500d2f44
kennytien <vandenplas00@gmail.com>
2026-03-12 15:17:44 +0800

    Simple travel planner website.
    Features
    
    - Add travel locations
    - Assign day
    - Delete schedule
    - Auto save with localStorage

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # travelplanner
+
+Simple travel planner website.
+
+Features
+
+- Add travel locations
+- Assign day
+- Delete schedule
+- Auto save with localStorage
+
+Deploy with GitHub Pages
--- script.js
+++ script.js
@@ -1,10 +1,53 @@
-function addPlace() {
+let list = document.getElementById("list")
 
-    let place = document.getElementById("place").value;
+let travelData = JSON.parse(localStorage.getItem("travel")) || []
 
-    let li = document.createElement("li");
-    li.textContent = place;
-
-    document.getElementById("list").appendChild(li);
-
+function save() {
+    localStorage.setItem("travel", JSON.stringify(travelData))
 }
+
+function render() {
+
+    list.innerHTML = ""
+
+    travelData.forEach((item, index) => {
+
+        let li = document.createElement("li")
+
+        li.innerHTML =
+        "Day " + item.day +
+        " - " + item.place +
+        '<span class="delete" onclick="removeItem(' + index + ')">✖</span>'
+
+        list.appendChild(li)
+
+    })
+}
+
+function addItem() {
+
+    let place = document.getElementById("place").value
+    let day = document.getElementById("day").value
+
+    if(place === "" || day === "") return
+
+    travelData.push({
+        day: day,
+        place: place
+    })
+
+    save()
+    render()
+
+    document.getElementById("place").value = ""
+}
+
+function removeItem(index) {
+
+    travelData.splice(index,1)
+
+    save()
+    render()
+}
+
+render()
