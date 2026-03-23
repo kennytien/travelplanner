3fbae524dbc17f62aea50e3740f15bf46a72c1ba
kennytien <vandenplas00@gmail.com>
2026-03-23 15:42:57 +0800

    fix map icon issue

--- script.js
+++ script.js
@@ -4,6 +4,14 @@ let map
 let markers = []
 let polyline
 
+delete L.Icon.Default.prototype._getIconUrl
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon-2x.png',
+  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-shadow.png'
+})
+
 /* -----------------------
    日期格式
 ----------------------- */
